ab91c57700f257ea53a241d2f65623b671cb1a99
lenanikulshyna <lena.nikulshyna@gmail.com>
2022-03-20 17:02:30 +0300

    first commit

---
+++ SoundEcho.java
@@ -0,0 +1,13 @@
+class Sound {
+	public static void main(String args[]) {
+		double time, dist;
+
+		time = 10; //секунд
+
+		//—корость звука 340 м/с
+				
+		dist = time / 2 * 340;
+
+		System.out.println("–ассто€ние до места вспышки молнии " + "составл€ет " + dist + " метров");
+	}
+}
