c6550593959e80fd427a1e5747298166ba2cf03d
lenanikulshyna <lena.nikulshyna@gmail.com>
2022-03-21 14:06:02 +0300

    change encoding

--- StrDemo.java
+++ StrDemo.java
@@ -1,6 +1,6 @@
 class StrDemo {
 	public static void main(String args[]) {
-		System.out.println("Первая строка\nВторая строка");
+		System.out.println("РџРµСЂРІР°СЏ СЃС‚СЂРѕРєР°\nР’С‚РѕСЂР°СЏ СЃС‚СЂРѕРєР°");
 		System.out.println("A\tB\tC");
 		System.out.println("D\tE\tF");
 	}
