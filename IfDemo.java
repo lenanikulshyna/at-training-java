6a2ae2f64eda518cea62be7e70d6fda7a6415ab4
lenanikulshyna <lena.nikulshyna@gmail.com>
2022-03-21 13:58:38 +0300

    change encoding

--- IfDemo.java
+++ IfDemo.java
@@ -1,31 +1,28 @@
 class IfDemo {
 	public static void main(String args[]) {
 		int a, b, c;
-		
+
 		a = 2;
 		b = 3;
 
-		if(a < b) System.out.println("a меньше b");
-		
-		if (a == b) System.out.println("Вы не должны увидеть этот текст");
+		if(a < b) System.out.println("a РјРµРЅСЊС€Рµ b");
+
+		if (a == b) System.out.println("Р’С‹ РЅРµ РґРѕР»Р¶РЅС‹ СѓРІРёРґРµС‚СЊ СЌС‚РѕС‚ С‚РµРєСЃС‚");
 
 		System.out.println();
 
 		c = a - b;
 
-		System.out.println("c содержит -1");
-		if(c >= 0) System.out.println("c - не отрицательное число");
-		if(c < 0) System.out.println("c - отрицательное число");
+		System.out.println("c СЃРѕРґРµСЂР¶РёС‚ -1");
+		if(c >= 0) System.out.println("c - РЅРµ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ");
+		if(c < 0) System.out.println("c - РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ");
 
 		System.out.println();
 
 		c = b - a;
 
-		System.out.println("c содержит 1");
-		if(c >=0) System.out.println("c - не отрицательное число");
-		if(c < 0) System.out.println("c - отрицательное число");
+		System.out.println("c СЃРѕРґРµСЂР¶РёС‚ 1");
+		if(c >=0) System.out.println("c - РЅРµ РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ");
+		if(c < 0) System.out.println("c - РѕС‚СЂРёС†Р°С‚РµР»СЊРЅРѕРµ С‡РёСЃР»Рѕ");
 	}
 }
-		
-
-		
