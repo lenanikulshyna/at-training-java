88ee905b5b23e184ea52389f24fc47bc6b9468e9
lenanikulshyna <lena.nikulshyna@gmail.com>
2022-03-21 22:10:46 +0300

    first commit

---
+++ ScopeDemo.java
@@ -0,0 +1,15 @@
+class ScopeDemo {
+  public static void main(String args[]) {
+    int x;
+
+    x = 10;
+if(x==10) {
+  int y = 20;
+
+  System.out.println("x и y: " + x + " " + y);
+  x = y*2;
+  }
+
+ System.out.println("x - это " + x);
+ }
+}
