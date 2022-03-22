234f851e65fa0db84a19f2b2cde10c65ff791d8c
lenanikulshyna <lena.nikulshyna@gmail.com>
2022-03-22 12:23:32 +0300

    first commit

---
+++ SideEffects.java
@@ -0,0 +1,16 @@
+class SideEffects {
+  public static void main(String args[]) {
+    int i;
+
+    i = 0;
+
+    if(false & (++i < 100))
+        System.out.println("Эта строка не будет отображаться");
+      System.out.println("Инструкция if выполняется: " + i);
+
+      if(false && (++i <100))
+        System.out.println("Эта строка не будет отображаться");
+      System.out.println("Инструкция if выполняется: " + i);
+
+  }
+}
